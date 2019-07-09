ee7d8800ebba80c36b38a89e4d2c06cfbb513c15
explorerneha <neha.bhushan@mavs.uta.edu>
2019-07-09 13:00:35 -0500

    Transferred calling of numbers_to word function into new function

--- venv/word_functions.py
+++ venv/word_functions.py
@@ -1,5 +1,5 @@
 import re
-
+import detectEnglish
 # digit_map is a dictionary data type to map numbers with alphabets according to mobile keypad notation
 digit_map = {
     '2': 'ABC',
@@ -26,7 +26,7 @@ letter_map = {
 # word_numbers function takes number as string input of phone number with delimeters removed and returns all possible combinations of 2 or more alhabet words that can be formed
 # word_numbers('543') returns an array of size 45 combinations of alphabets represented by number input like ['JG', 'JH', 'JI', 'KG', 'KH', 'KI','KHF', 'KID', 'KIE'.... ]along with index in number input of the first alphabet of the word created
 
-def number_to_words(input):
+def word_numbers(input):
   string_input = input
 
   # flag, flag2 used to offset single character words and empty string from array of combinations
@@ -78,6 +78,58 @@ def number_to_words(input):
 
 #debug check
 #print(len(number_to_words('543')[0]))
+
+# words_to_number function takes telephone number as input and returns wordified telephone number
+def number_to_words(input):
+    
+    # input phone number
+    phone_number = input
+
+    # removing delimiters from the phone number extracting only numbers
+    number = re.findall("\d+", phone_number)
+    number = ''.join(number)
+
+    # find all possible combinations that can be formed using 2 or more letters from the wordified phone number
+    [combinations, f_index] = word_numbers(number)
+
+    # create empty list words to store list of english words
+    words = []
+
+    # create empty list word_index to store index of wordified alphabet respective to the english word in words list
+    word_index = []
+
+    # iterate in all possible combinations to find english words
+    for word in combinations:
+
+        # detect is true when word in combinations list belongs to the english dictionary
+        detect = detectEnglish.isEnglish(word)
+
+        # if english word detected, append english word in the list of words
+        if detect:
+            words.append(word)
+
+            # note index of the first letter of the word from combinations
+            word_index.append(combinations.index(word))
+
+    # consider longest word from words list to replace numbers in the telephone number
+    longest_word = max(words, key=len)
+
+    # find index of the longest word to use while replacing relative number in telephone number
+    longest_word_index = words.index(longest_word)
+
+    # changing the index value to print to get into telephone number with hyphen format
+    final_index = f_index[word_index[longest_word_index]]
+    if final_index > 6:
+        final_index += 3
+    elif final_index > 3:
+        final_index += 2
+    elif final_index > 0:
+        final_index += 1
+
+    # replacing numbers in telephone number with their wordified version
+    answer = str(phone_number[0:final_index]) + longest_word + phone_number[
+                                                               final_index + len(longest_word) + 1:len(phone_number)]
+    return answer
 
 # words_to_number function takes wordified telephone number as input and returns numbered telephone number
 def words_to_number(input):
