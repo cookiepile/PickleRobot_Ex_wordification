cac5f83e405e5af764dc72621b1b8a3a783e2651
explorerneha <neha.bhushan@mavs.uta.edu>
2019-07-09 12:43:27 -0500

    New function words_to_number() added for question 2 of exercise: words_to_number(), which does the reverse of the above function (for example, the output of words_to_number("1-800-PAINTER") should be "1-800-724-6837")

--- venv/word_functions.py
+++ venv/word_functions.py
@@ -1,3 +1,5 @@
+import re
+
 # digit_map is a dictionary data type to map numbers with alphabets according to mobile keypad notation
 digit_map = {
     '2': 'ABC',
@@ -8,6 +10,17 @@ digit_map = {
     '7': 'PQRS',
     '8': 'TUV',
     '9': 'WXYZ',
+}
+
+letter_map = {
+    ('A','B','C')     : '2',
+    ('D','E','F')     : '3',
+    ('G','H','I')     : '4',
+    ('J','K','L')     : '5',
+    ('M','N','O')     : '6',
+    ('P','Q','R','S') : '7',
+    ('T','U','V')     : '8',
+    ('W','X','Y','Z') : '9',
 }
 
 # word_numbers function takes number as string input of phone number with delimeters removed and returns all possible combinations of 2 or more alhabet words that can be formed
@@ -65,3 +78,49 @@ def number_to_words(input):
 
 #debug check
 #print(len(number_to_words('543')[0]))
+
+# words_to_number function takes wordified telephone number as input and returns numbered telephone number
+def words_to_number(input):
+
+    # remove delimiters and words from telephone number
+    original_numbers=''.join(re.findall("\d+", input))
+
+    # extract words with hyphen from telephone number
+    wordified_numbers = ''.join(re.findall("\D+", input))
+
+    # remove hyphen from the wordified_numbers
+    word_array = [c for c in wordified_numbers if c not in ' -']
+    word=''.join(word_array)
+
+    # create array to store number converted from the wordified phone number
+    converted_number = []
+
+    # convert each character of word to number by mapping from dictionary letter_map
+    for char in word:
+        number = (next(v for k, v in letter_map.items() if char in k))
+        converted_number.append(number)
+    numbers = ''.join(converted_number)
+
+    # find index in the input of the first numbr that was converted from word
+    for i in range(len(input)):
+        if word_array[0]==input[i]:
+            final_index=i
+
+            # change the index to combine the converted number with original_numbers from input
+            if i <5:
+                final_index -= 1
+            elif i< 9:
+                final_index -= 2
+            else:
+                final_index -= 3
+            break
+
+    # combine the converted number with original_numbers from input and store in phone_number
+    phone_number=original_numbers[0:final_index]+numbers[0:len(numbers)]+original_numbers[final_index:len(original_numbers)]
+
+    # final_phone_number is the phone_number in telephone number format with hyphens
+    final_phone_number=phone_number[0]+'-'+phone_number[1:4]+'-'+phone_number[4:7]+'-'+phone_number[7:11]
+    return final_phone_number
+
+# debug check
+# print(words_to_number('1-232-324-923Z'))
